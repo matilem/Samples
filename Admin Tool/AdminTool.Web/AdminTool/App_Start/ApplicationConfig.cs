5760cb0a6c66321b1a53aebb2e9a5a58a21a66c2
Megan <matilem@gmail.com>
2019-07-30 22:24:48 -0500

    Create ApplicationConfig.cs
    Initial Check-in

---
+++ Admin Tool/AdminTool.Web/AdminTool/App_Start/ApplicationConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AdminTool
+{
+    public class ApplicationConfig
+    {
+        public static string JQueryCdn => "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js";
+
+        public static string BootstrapCdn => "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css";
+    }
+}
